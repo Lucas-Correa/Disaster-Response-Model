d8e3610106bda1f343bfc181eccec59b8404a04c
Lucas-Correa <lukec.correa@gmail.com>
2020-08-19 23:07:57 -0300

    Fill ETL's
    Filled the basic functions structures with the code

--- process_data.py
+++ process_data.py
@@ -1,16 +1,42 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+
+    messages = pd.read_csv('messages.csv')
+    categories = pd.read_csv('categories.csv')
+    df = categories.merge(messages,left_on='id',right_on='id')
+
+    categories = categories.categories.str.split(';',expand=True)
+    category_colnames = list(row.apply(lambda x : x[:-2]))
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
+
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    df.drop('categories',axis=1,inplace=True)
+    df = pd.concat([df,categories],axis=1)
+
+    return df
 
 
 def clean_data(df):
-    pass
 
+    df.drop_duplicates(inplace=True)
+
+    #rows that have nan for all categorical columns
+    to_drop = df[df.iloc[: , 4:].isnull().all(axis=1)].index
+    df.drop(to_drop,inplace=True)
 
 def save_data(df, database_filename):
-    pass  
+
+    engine = create_engine('sqlite:///F8MessegesCat.db')
+    df.to_sql('MgsCat', engine, index=False)
 
 
 def main():
@@ -24,12 +50,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- train_classifier.py
+++ train_classifier.py
@@ -1,24 +1,83 @@
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
 
+import pandas as pd
+import re
+import pickle
+
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///F8MessegesCat.db')
+    df = pd.read_sql_table('MgsCat',engine)
 
+    X = df.message.values
+    y = df.drop(['message','genre','related','id','original'],axis=1)
+
+    return X, y.values, y.columns
 
 def tokenize(text):
-    pass
+
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
 
+    pipeline = Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                ])
+
+    parameters = {
+    'vect__ngram_range': ((1, 1),(1,2)),
+    'vect__max_df': (0.5, 0.75, 1.0),
+    'vect__max_features': (None, 5000, 10000),
+    'tfidf__use_idf': (True, False),
+    'clf__estimator__n_estimators': [50, 100, 200],
+    'clf__estimator__min_samples_split': [2, 3, 4],
+    }
+
+    cv =  GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+
+    with open(model_filepath + 'model.pickle', 'wb') as f:
+    pickle.dump(model, f)
 
 
 def main():
@@ -27,13 +86,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
